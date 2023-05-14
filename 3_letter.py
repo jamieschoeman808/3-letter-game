9ab785a0eb2ddbb9c1b71234d9874bde00c7948d
Jamie Schoeman <jamieschoeman808@gmail.com>
2023-05-14 22:39:44 +0200

    Separated points and letters

--- 3_letter.py
+++ 3_letter.py
@@ -1,16 +1,15 @@
-wordList = []
+#This currently accesses a file containing all 3 letter words according to the collins dictionary. 
+wordList=[] #This list will store all the words
+pointList = [] #This list will store all the point values. Although we won't be using it now, we will keep it in case we require it in the future
+with open("words.txt", "r") as wordsRaw: #open the file and save it to the file object wordsRaw
+    words = wordsRaw.readline() #The readline method returns a list
+    words = words.strip() #We use strip to remove any potential whitepace at the front or end of the string
 
-with open("words.txt", "r") as words:
-    
-    word = words.readlines()
-    wordList.append(word)
+    for i in words:
+        try:
+           pointList.append(int(i)) #We add all number values to the pointList
+        except ValueError: #if the int function is called on a string and an int is not returned, we receive a ValueError
+            wordList.append(i)#we know its a letter and add it to wordList
 
 print(wordList)
-#def separate_word(wordList, word):
-    #for i in range(4):
-        #wordList.append(word[i])
-
-    #print(wordList)
-
-#separate_word(wordList, word)    
 
