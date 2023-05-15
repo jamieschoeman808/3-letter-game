25b6892a1a02653f3f4c8da972aab339dac4c28a
Jamie Schoeman <jamieschoeman808@gmail.com>
2023-05-15 22:22:04 +0200

    grouping words

--- 3_letter.py
+++ 3_letter.py
@@ -1,5 +1,6 @@
 #This currently accesses a file containing all 3 letter words according to the collins dictionary. 
 wordList=[] #This list will store all the words
+threeLetterWordList = []
 pointList = [] #This list will store all the point values. Although we won't be using it now, we will keep it in case we require it in the future
 with open("words.txt", "r") as wordsRaw: #open the file and save it to the file object wordsRaw
     words = wordsRaw.readline() #The readline method returns a list
@@ -11,5 +12,15 @@ with open("words.txt", "r") as wordsRaw: #open the file and save it to the file 
         except ValueError: #if the int function is called on a string and an int is not returned, we receive a ValueError
             wordList.append(i)#we know its a letter and add it to wordList
 
-print(wordList)
+
+for i in range(1007):
+    word = wordList[0]+wordList[1]+wordList[2]
+    threeLetterWordList.append(word)
+    wordList.remove(wordList[0])
+    wordList.remove(wordList[0])
+    wordList.remove(wordList[0])
+
+
+print(threeLetterWordList)
+
 
